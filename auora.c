6ea3a9bca9030142b891b9788653ae15b266b918
auroale1 <127745909+auroale1@users.noreply.github.com>
2023-03-14 11:48:10 +0100

    Add files via upload

---
+++ auora.c
@@ -0,0 +1,34 @@
+    //SCRIVERE UN PROGRAMMA CHE CHIEDA ALL'UTENTE DI INSERIRE 
+    //UN INT ETA E DICA SE E'MAGGIORENNE O MENO 
+    
+
+    #include <stdio.h>
+
+    int main()
+    {
+        char sesso;
+        printf("scrivi il tuo sesso: ");
+        scanf(" %c", &sesso);
+        if (sesso == 'm')
+        {
+            printf("sei maschio\n");
+        }
+        else if (sesso == 'f')
+        {
+            printf("sei femmina\n");
+        }
+        else if (sesso =='n')
+        {
+                printf("non sei ne maschio ne femmina\n");   
+        }
+        else 
+        {
+            printf( "in qualunque altro caso stampi un messaggio di errore\n");
+        }
+    }
+        
+        
+
+    
+        
+        
